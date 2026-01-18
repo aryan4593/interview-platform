f36ea8e62db9fe1587f5cb4233dbb86f977e20c0
Aryan4593 <aryanpungle45@gmail.com>
2026-01-18 14:27:21 +0530

    Validation check added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,8 +3,10 @@ import { ENV } from "./env.js";
 
 export const connectDB = async ()=>{
     try {
+       if(!ENV.DB_URL){
+        throw new Error('DB URL IS UNDEFINED')
+       }
        const conn =  await mongoose.connect(ENV.DB_URL);
-
        console.log("connected to MongoDB", conn.connection.host)
     } catch (error) {
         console.error("X Error in connecting MongoDB ", error);
