55d7d3f8b9466736a066a9c53666c4916e4b5471
Aryan4593 <aryanpungle45@gmail.com>
2026-02-01 17:30:49 +0530

    completed home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import {
   SignedOut,
   SignedIn,
@@ -12,15 +10,17 @@ import {
 import {Navigate, Route, Routes} from "react-router"
 import HomePage from "./pages/homePage.jsx";
 import ProblemPage from "./pages/ProblemPage.jsx";
+import DashBoardPage from "./pages/DashBoardPage.jsx";
 function App() {
   const [count, setCount] = useState(0);
-  const {isSignedIn} = useUser()
+  const {isSignedIn, isLoaded} = useUser();
+  if(!isLoaded) return null;
   return (
     <>
-    <h1 className="text-red-50">Welcome to the app</h1>
     <Routes>
 
-      <Route path="/" element={<HomePage/>}/>
+      <Route path="/" element={!isSignedIn ?  <HomePage/> : <Navigate to={'/dashboard'}/>}/>
+      <Route path="/dashboard" element={isSignedIn ?  <DashBoardPage/> : <HomePage/>}/>
       <Route path="/problems" element={isSignedIn ?<ProblemPage/> : <Navigate to={"/"}/>}/>
 
     </Routes>
